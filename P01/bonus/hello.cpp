a61eb820eda1bb6f56828939697f1adc67ea8f74
Bhuwan Shrestha <bxs8072@mavs.uta.edu>
2020-08-30 16:45:10 -0400

    P01 fc First Commit

--- P01/bonus/hello.cpp
+++ P01/bonus/hello.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello John!";
+    string name;
+    cout << "Enter your name: \n";
+    cin >> name;
+    cout << "Hello " + name + " !\n";
     return 0;
 }
